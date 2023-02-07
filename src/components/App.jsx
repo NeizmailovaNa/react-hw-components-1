299e0436b71a8be13056287bbc80b8981929453c
Neizmailova Natalia <natata.neizmailova@gmail.com>
2023-02-07 11:48:09 +0200

    some-bags

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
-import user from 'data/user.json'
-import data from 'data/data.json'
-import friends from 'data/friends.json'
-import transactions from 'data/transactions.json'
+import user from '../data/user.json'
+import data from '../data/data.json'
+import friends from '../data/friends.json'
+import transactions from '../data/transactions.json'
 
 
 
@@ -16,7 +16,9 @@ export const App = () => {
       style={{
         height: '100vh',
         display: 'flex',
-        justifyContent: 'center',
+        flexDirection: 'column',
+        gap: 20,
+        justifyContent: 'flex-start',
         alignItems: 'center',
         fontSize: 40,
         color: '#010101'
